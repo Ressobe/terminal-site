c55698ce91ce16dc5df343696f209baae18617c4
Ressobe <bartosz.sobina@tutanota.com>
2022-07-09 20:09:18 +0200

    New banner and version info

--- js/variables.js
+++ js/variables.js
@@ -1,15 +1,17 @@
 var github = "https://github.com/Ressobe";
 var yt = "https://www.youtube.com/channel/UCcWXlahMIEBY9WR11VVELuQ";
+var version = "1.0";
 
-var left_side = "<div class=left_side>"
-var right_side = "<div class=right_side>"
-var close_div = "</div>"
-var close_right = close_div + right_side
+var left_side = "<div class=left_side>";
+var right_side = "<div class=right_side>";
+var close_div = "</div>";
+var close_right = close_div + right_side;
 
-const help= [
+const commandsArray = ['help', 'projects', 'socials', 'neofetch', 'banner', 'cmatrix', 'about', 'clear'];
+
+const help = [
 	  '<br>',
 	  left_side + 'clear' + close_right + ' clear terminal screen' + close_div,
-	  left_side + 'exit' + close_right + 'close this page' + close_div ,
 	  left_side + 'projects' + close_right +  'display my projects' + close_div,
 	  left_side + 'socials'  + close_right + 'links to my socials' + close_div ,
 	  left_side + 'neofetch' + close_right + 'display info about my hardware' + close_div,
@@ -25,15 +27,16 @@ const socials = [
 ];
 
 const banner = [
-	  '<br>',
-	  '.########..########..######...######...#######..########..########',
-	  '.##.....##.##.......##....##.##....##.##.....##.##.....##.##......',
-	  '.##.....##.##.......##.......##.......##.....##.##.....##.##......',
-	  '.########..######....######...######..##.....##.########..######..',
-	  '.##...##...##.............##.......##.##.....##.##.....##.##......',
-	  '.##....##..##.......##....##.##....##.##.....##.##.....##.##......',
-	  '.##.....##.########..######...######...#######..########..########',
-	  '<br>'
+	'<br>',
+	'██████╗░███████╗░██████╗░██████╗░█████╗░██████╗░███████╗',
+	'██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝',
+	'██████╔╝█████╗░░╚█████╗░╚█████╗░██║░░██║██████╦╝█████╗░░',
+	'██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗██║░░██║██╔══██╗██╔══╝░░',
+	'██║░░██║███████╗██████╔╝██████╔╝╚█████╔╝██████╦╝███████╗',
+	'╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░░╚════╝░╚═════╝░╚══════╝' + ' ' + version,
+	'<br>',
+	"Type 'help' to see list of available commands",
+	'<br>'
 ]
 
 const hardwareSoftwareInfo = [
@@ -45,7 +48,3 @@ const hardwareSoftwareInfo = [
 	left_side + 'Terminal Font:' + close_right + 'Hack Nerd Font' + close_div,
 	'<br>'
 ];
-
-const commandsArray = ['help', 'projects', 'socials', 'neofetch', 'banner', 'cmatrix', 'whois', 'clear', 'exit' ];
-
-const about = "";
